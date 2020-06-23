20acedf2937d05384751fcc3972db98679d7de5e
ITO Daisuke <ito@wacul.co.jp>
2020-06-23 17:47:38 +0900

    typo

--- ecr-addtag/main.py
+++ ecr-addtag/main.py
@@ -17,16 +17,16 @@ class AwsUtils(object):
         session = Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
         self.ecr = session.client('ecr')
 
-    def get_image_manifest(self, registory_id: str, repository: str, source_tag: str) -> str:
-        response = self.ecr.batch_get_image(registryId=registory_id, repositoryName=repository, imageIds=[{'imageTag': source_tag}])
+    def get_image_manifest(self, registry_id: str, repository: str, source_tag: str) -> str:
+        response = self.ecr.batch_get_image(registryId=registry_id, repositoryName=repository, imageIds=[{'imageTag': source_tag}])
         failures = response.get('failures')
         if failures:
             raise Exception("Cluster '%s' is %s" % (cluster, failures[0].get('reason')))
         return response.get('images')[0].get('imageManifest')
 
-    def add_tag(self, registory_id: str, repository: str, add_tag: str, image_manifest:str):
+    def add_tag(self, registry_id: str, repository: str, add_tag: str, image_manifest:str):
         try:
-            response = self.ecr.put_image(registryId=registory_id, repositoryName=repository, imageManifest=image_manifest, imageTag=add_tag)
+            response = self.ecr.put_image(registryId=registry_id, repositoryName=repository, imageManifest=image_manifest, imageTag=add_tag)
         except ClientError as e:
             if e.response['Error']['Code'] == 'ImageAlreadyExistsException':
                 logger.info('Image Tag already Exists')
@@ -53,12 +53,12 @@ if __name__ == '__main__':
     parser.add_argument('--key', action=EnvDefault, envvar='AWS_ACCESS_KEY_ID')
     parser.add_argument('--secret', action=EnvDefault, envvar='AWS_SECRET_ACCESS_KEY')
     parser.add_argument('--region', default='us-east-1', action=EnvDefault, envvar='AWS_DEFAULT_REGION')
-    parser.add_argument('--registory-id', required=True, action=EnvDefault, envvar='AWS_REGISTORY_ID')
+    parser.add_argument('--registry-id', required=True, action=EnvDefault, envvar='AWS_REGISTRY_ID')
     parser.add_argument('--repository', required=True, action=EnvDefault, envvar='REPOSITORY_NAME')
     parser.add_argument('--source-tag', required=True, action=EnvDefault, envvar='SOURCE_TAG')
     parser.add_argument('--add-tag', required=True, action=EnvDefault, envvar='ADD_TAG')
     argp = parser.parse_args()
     
     awsutils = AwsUtils(access_key=argp.key, secret_key=argp.secret, region=argp.region)
-    image_manifest = awsutils.get_image_manifest(registory_id=argp.registory_id, repository=argp.repository, source_tag=argp.source_tag)
-    awsutils.add_tag(registory_id=argp.registory_id, repository=argp.repository, add_tag=argp.add_tag, image_manifest=image_manifest)
+    image_manifest = awsutils.get_image_manifest(registry_id=argp.registry_id, repository=argp.repository, source_tag=argp.source_tag)
+    awsutils.add_tag(registry_id=argp.registry_id, repository=argp.repository, add_tag=argp.add_tag, image_manifest=image_manifest)
